3ab817cf70b5e51f8503a3e9dae9f1e6db5d2312
Fabian Lim <fabian_lim@ida-gds.com>
2018-05-15 17:14:58 +0800

    modify code to add env

--- hello-world/app.py
+++ hello-world/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
+
+HUMAN_USERNAME = os.getenv('HUMAN_USERNAME', 'World')
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! - Nectarians"
+    return "Hello " + HUMAN_USERNAME + "! - Nectarians"
 
 
 if __name__ == '__main__':
